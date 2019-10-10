161685f16fb95a23c08f9a7c3e9e099ffb8dc3a1
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-10-10 21:49:40 +0200

    fix

--- mvc tutorial/src/com/springtutorial/spring/web/dao/Offer.java
+++ mvc tutorial/src/com/springtutorial/spring/web/dao/Offer.java
@@ -13,9 +13,9 @@ public class Offer {
 	@Size(min = 5, max = 100, message = "Name must be between 5 and 100 characters.")
 	private String name;
 	@NotNull
-	@Pattern(regexp = ".*\\@.*\\..*", message = "This is not a valid email adress.")
+	@Pattern(regexp =".*\\@.*\\..*", message = "This is not a valid email adress.")
 	private String emial;
-	@Size(min = 20, max = 100,  message = "Email must be between 20 and 100 characters.")
+	@Size(min = 20, max = 100,  message = "Text must be between 20 and 100 characters.")
 	private String text;
 
 	public Offer() {
