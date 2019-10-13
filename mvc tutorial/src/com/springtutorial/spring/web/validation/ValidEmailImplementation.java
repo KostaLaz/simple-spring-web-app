a59d27ad597bd2004fcd7ed19e7b997bc774541b
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-10-13 19:15:02 +0200

    Creating a Custom Validation Annotation

---
+++ mvc tutorial/src/com/springtutorial/spring/web/validation/ValidEmailImplementation.java
@@ -0,0 +1,7 @@
+package com.springtutorial.spring.web.validation;
+
+import javax.validation.ConstraintValidator;
+
+public class ValidEmailImplementation implements ConstraintValidator<ValidEmail, String>{
+
+}
