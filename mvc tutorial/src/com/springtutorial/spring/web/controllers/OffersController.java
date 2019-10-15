707a9f77ea0a1bd38597d50eb57a578db8696c7b
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-10-15 21:56:18 +0200

    Exception Handling in Spring MVC

--- mvc tutorial/src/com/springtutorial/spring/web/controllers/OffersController.java
+++ mvc tutorial/src/com/springtutorial/spring/web/controllers/OffersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -36,6 +37,11 @@ public class OffersController {
 
 		return "offers";
 
+	}
+	
+	public String handleDataBaseExeption(DataAccessException ex) {
+		
+		return "error";
 	}
 
 	@RequestMapping("/test")
