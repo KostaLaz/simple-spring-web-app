0f52bc48731702021a8b2300f93ab9c6d87f1a1c
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-10-15 22:27:38 +0200

    Global Exception Handling in Spring MVC

---
+++ mvc tutorial/src/com/springtutorial/spring/web/controllers/DatabaseErrorHandler.java
@@ -0,0 +1,18 @@
+package com.springtutorial.spring.web.controllers;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DatabaseErrorHandler {
+	
+	
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDataBaseExeption(DataAccessException ex) {
+		
+		return "error";
+	}
+
+
+}
