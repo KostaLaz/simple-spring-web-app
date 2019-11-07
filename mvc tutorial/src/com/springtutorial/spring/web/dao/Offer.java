c0f10c1dd77bdb1e91f59a4fd2360717742daafe
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-11-07 17:20:02 +0100

    Using Property File Values in JSPs

--- mvc tutorial/src/com/springtutorial/spring/web/dao/Offer.java
+++ mvc tutorial/src/com/springtutorial/spring/web/dao/Offer.java
@@ -13,13 +13,13 @@ public class Offer {
 	
 	private int id;
 	
-	@Size(min = 5, max = 100, message = "Name must be between 5 and 100 characters.")
+	@Size(min = 5, max = 100)
 	private String name;
 	@NotNull
 	//@Pattern(regexp = ".*\\@.*\\..*", message = "This is not a valid email adress")
-	@ValidEmail(min = 6, message = "This email adress is not valid")
+	@ValidEmail(min = 6)
 	private String email;
-	@Size(min = 20, max = 100,  message = "Text must be between 20 and 100 characters.")
+	@Size(min = 20, max = 100)
 	private String text;
 
 	public Offer() {
