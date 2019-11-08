41ec144b819207e9339cb38b75983c9810e735b0
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-11-08 20:59:22 +0100

    Method-Level Access Control

--- mvc tutorial/src/com/springtutorial/spring/web/service/UsersService.java
+++ mvc tutorial/src/com/springtutorial/spring/web/service/UsersService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.springtutorial.spring.web.dao.Offer;
@@ -34,6 +35,7 @@ public class UsersService {
 		return usersDao.exists(username);
 	}
 
+	@Secured("ROLE_ADMIN")
 	public List<User> getAllUsers() {
 		
 		
