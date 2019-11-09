d9561bbae01a39c329e97983ad5836df8e858298
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-11-09 19:09:43 +0100

    Catching Secure Annotation Violations

--- mvc tutorial/src/com/springtutorial/spring/web/controllers/ErrorHandler.java
+++ mvc tutorial/src/com/springtutorial/spring/web/controllers/ErrorHandler.java
@@ -1,11 +1,12 @@
 package com.springtutorial.spring.web.controllers;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class DatabaseErrorHandler {
+public class ErrorHandler {
 	
 	
 	@ExceptionHandler(DataAccessException.class)
@@ -15,5 +16,11 @@ public class DatabaseErrorHandler {
 		return "error";
 	}
 
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public String handleAccessDenidExeption(AccessDeniedException ex) {
+		
+		return "denied";
+	}
 
 }
